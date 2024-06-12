68d08ddf139fec68383e89f1ce43f0629b7efb70
yy <kim41101@naver.com>
2024-06-12 23:58:25 +0900

    Revise prompt
    (1) change to gpt-4o
    (2) adjust word count based on Lexile level

--- src/components/api/TranslateSettings.tsx
+++ src/components/api/TranslateSettings.tsx
@@ -2,8 +2,41 @@ import { Setting } from "../../Contexts";
 import prompts from "./Prompts";
 
 function TranslateSetting(setting: Setting | null) {
+
+  let word_num: number;
+
+
   if(setting === null) return "";
-  const prompt = "Generate an hypothetical" + setting.format + "which has lexile level" + setting.li.toString() + "and ends. The text should contain the following words: " + setting.keywords.join(", ") + ". The keywords may be in different languages, but use the English translations for generation.";
+ 
+ 
+  switch(setting.li) {
+    case 100:
+      word_num = 50;
+      break;
+    case 300:
+      word_num = 150;
+      break;  
+    case 500:
+      word_num = 300;
+      break; 
+    case 700:
+      word_num = 400;
+      break;
+    case 900:
+      word_num = 600;
+      break;
+    case 1100:
+      word_num = 800;
+      break;
+    case 1500:
+      word_num = 1000;
+      break;
+    default:
+      word_num = 500;
+  }
+
+  const prompt = "Generate a "+ word_num + "-word " + setting.format + "which has lexile level " + setting.li.toString() + "and title flanked by <>. The text should contain the following words in English: " + setting.keywords.join(", ") + "."
+
   return prompts(prompt);
 }
 
